4d47d42520dc05400bc17dfb6015473e6164910b
Fred Plas <fred.plas@gmail.com>
2023-07-12 22:32:54 +0200

    precision de la version de Kubernetes

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -67,6 +67,7 @@ resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
 resource "aws_eks_cluster" "demo" {
   name     = var.cluster_name
   role_arn = aws_iam_role.demo-cluster.arn
+  version  = "1.27"
 
   vpc_config {
     security_group_ids = [aws_security_group.demo-cluster.id]
